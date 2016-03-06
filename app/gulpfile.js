10a9b8fd16e5a1018c84b20534d155c53645c1fb
kartik <rkartik1992@gmail.com>
2016-03-06 20:42:29 +0530

    Learning Gulp #5 - Compiling Sass With Gulp

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass');
 
 
 // uglify scripts
@@ -10,16 +11,22 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('build/js'));
 });
 
+// watches js
+gulp.task('watchScripts', function() {
+    gulp.watch('js/*.js', ['scripts']);
+});
 
 // styles
 gulp.task('styles', function() {
-    console.log('runs styles');
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('build/css'));
 });
 
-// watches js
-gulp.task('watchScripts', function() {
-	gulp.watch('js/*.js', ['scripts']);
+gulp.task('sass:watch', function() {
+    gulp.watch('./sass/**/*.scss', ['styles']);
 });
+
 
 
 gulp.task('default', ['scripts', 'styles', 'watchScripts']);
